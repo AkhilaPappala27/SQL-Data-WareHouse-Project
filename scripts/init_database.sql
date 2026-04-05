f8f12b3076b12f03ed1adb1d39f8ab78f1193f22
Pappala Akhila <ugs23088_aids.akhila@cbit.org.in>
2026-04-05 20:12:48 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,24 @@
+-- Create Database if not exists
+DROP DATABASE IF EXISTS DataWareHouse;
+CREATE DATABASE DataWareHouse;
+
+-- Use database
+USE DataWareHouse;
+
+/*
+Purpose:
+This database is created to build a Data Warehouse for storing and analyzing data.
+It supports data processing, transformation, and analytical queries.
+
+Note:
+MySQL does not support separate schemas inside a database,
+so we use naming conventions:
+
+bronze_* → raw data tables (bronze_customers, bronze_orders)
+silver_* → cleaned data tables (silver_customers, silver_orders)
+gold_* → final analytics tables (gold_sales, gold_reports)
+
+Warning:
+Dropping the database will permanently delete all tables and data.
+Use DROP DATABASE carefully.
+*/
