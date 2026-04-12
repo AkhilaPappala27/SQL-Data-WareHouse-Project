f1bc51e265c1631ba296c83213ebe2d6f1cbdfc0
Pappala Akhila <ugs23088_aids.akhila@cbit.org.in>
2026-04-12 18:27:34 +0530

    Update run_bronze.bat

--- scripts/bronze/run_bronze.bat
+++ scripts/bronze/run_bronze.bat
@@ -1,7 +1,6 @@
-/* 
-Purpose:
-This .bat file is a one‑click automation: It runs your bronze loading SQL script against the datawarehouse database, updates all staging tables from local CSVs, and then displays a confirmation message so you know the refresh completed successfully.
-*/
+::Purpose:
+::This .bat file is a one‑click automation: It runs your bronze loading SQL script against the datawarehouse database, updates all staging tables from local CSVs, and then displays a confirmation message so you know the refresh completed successfully.
+
 @echo off
 REM Run load_bronze.sql against the datawarehouse database
 
