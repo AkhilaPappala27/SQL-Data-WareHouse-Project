751b2297dd5eec54a55e06ad9e034a9e82ebe581
Pappala Akhila <ugs23088_aids.akhila@cbit.org.in>
2026-04-29 18:54:23 +0530

    Update run_bronze.bat

--- scripts/bronze/run_bronze.bat
+++ scripts/bronze/run_bronze.bat
@@ -2,11 +2,14 @@
 ::This .bat file is a one‑click automation: It runs your bronze loading SQL script against the datawarehouse database, updates all staging tables from local CSVs, and then displays a confirmation message so you know the refresh completed successfully.
 
 @echo off
-REM Run load_bronze.sql against the datawarehouse database
 
+echo Creating Bronze Tables...
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p datawarehouse < create_bronze_tables.sql
+
+echo Running Bronze Load...
 "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -p datawarehouse < load_bronze.sql
 
 echo ============================================
-echo Tables in 'datawarehouse' have been updated
+echo Bronze Layer Loaded Successfully
 echo ============================================
 pause
